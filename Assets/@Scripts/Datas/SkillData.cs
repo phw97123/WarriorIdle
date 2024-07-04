c27c0d0326c5521347d468db8b09334337d431ab
hwon <132995834+phw97123@users.noreply.github.com>
2024-07-04 14:51:51 +0900

    [UPDATE] Skill Data

--- Assets/@Scripts/Datas/SkillData.cs
+++ Assets/@Scripts/Datas/SkillData.cs
@@ -1,13 +1,43 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SkillData
 {
     public SkillDataSO BaseData { get; private set; }
 
-    public int level;
+    public int level; 
     public int quantity;
+    public int maxQuantity;
     public bool isEquipped;
     public int damage; 
+
+    public SkillData(SkillDataSO baseData)
+    {
+        BaseData = baseData;
+
+        level = 1;
+        quantity = 0;
+        maxQuantity = 4; 
+        isEquipped = false;
+        damage = BaseData.damage; 
+    }
+
+    public void CheckLevel()
+    {
+        while(quantity>=maxQuantity)
+        {
+            LevelUp(); 
+            quantity -= maxQuantity; 
+        }
+    }
+
+    public void LevelUp()
+    {
+        level++;
+        maxQuantity += 2;
+        damage += level * 3; 
+    }
 }
